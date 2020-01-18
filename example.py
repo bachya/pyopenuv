baed5749b72bbd300b62a4f65d21ab7033776139
Aaron Bach <bachya1208@gmail.com>
2020-01-18 10:58:10 -0700

    Cleaned up example

--- example.py
+++ example.py
@@ -1,41 +1,41 @@
 """Run an example script to quickly test."""
 import asyncio
+import logging
 
 from aiohttp import ClientSession
 
 from pyopenuv import Client
 from pyopenuv.errors import OpenUvError
 
+_LOGGER = logging.getLogger(__name__)
+
+API_KEY = "<API_KEY>"
+LATITUDE = 39.7974509
+LONGITUDE = -104.8887227
+ALTITUDE = 1609.3
+
 
 async def main() -> None:
     """Create the aiohttp session and run the example."""
+    logging.basicConfig(level=logging.INFO)
     async with ClientSession() as websession:
-        await run(websession)
+        try:
+            # Create a client:
+            client = Client(API_KEY, LATITUDE, LONGITUDE, websession, altitude=ALTITUDE)
 
+            # Get current UV info:
+            _LOGGER.info("CURRENT UV DATA:")
+            _LOGGER.info(await client.uv_index())
 
-async def run(websession: ClientSession):
-    """Run."""
-    try:
-        # Create a client:
-        client = Client(
-            "<API_KEY>", 39.7974509, -104.8887227, websession, altitude=1609.3
-        )
+            # Get forecasted UV info:
+            _LOGGER.info("FORECASTED UV DATA:")
+            _LOGGER.info(await client.uv_forecast())
 
-        # Get current UV info:
-        print("CURRENT UV DATA:")
-        print(await client.uv_index())
-
-        # Get forecasted UV info:
-        print()
-        print("FORECASTED UV DATA:")
-        print(await client.uv_forecast())
-
-        # Get UV protection window:
-        print()
-        print("UV PROTECTION WINDOW:")
-        print(await client.uv_protection_window())
-    except OpenUvError as err:
-        print(err)
+            # Get UV protection window:
+            _LOGGER.info("UV PROTECTION WINDOW:")
+            _LOGGER.info(await client.uv_protection_window())
+        except OpenUvError as err:
+            _LOGGER.info(err)
 
 
 asyncio.get_event_loop().run_until_complete(main())
